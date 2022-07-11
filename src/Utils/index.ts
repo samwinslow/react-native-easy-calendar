edcd5ea33ec170f6c6d2cf79eebbd4d521a14f92
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2022-07-11 12:07:50 -0400

    chore: update podfile lock && rm unused code

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -3,4 +3,3 @@ export { default as getSurroundingTimeUnits } from './getSurroundingTimeUnits';
 export { default as getExtraDays } from './getExtraDays';
 export { default as clamp } from './clamp';
 export { default as dateRange } from './dateRange';
-export { default as checkChangedProps } from './checkChangedProps';
